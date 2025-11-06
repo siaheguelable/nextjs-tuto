54e0574b6f5be57db3dfdee9260576822df82b1d
siaheguelable <siaheguelable@gmail.com>
2025-11-06 15:03:07 +0100

    lusitana

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Lusitana } from 'next/font/google';
+
+// Export a named `lusitana` font instance used across the UI components.
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400'],
+  display: 'swap',
+});
