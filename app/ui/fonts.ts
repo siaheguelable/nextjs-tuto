3a345ffcea8ae46282d3f91619dfdb258ba6bff4
siaheguelable <siaheguelable@gmail.com>
2025-11-07 14:22:33 +0100

    assigment one finished

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,11 @@
 import { Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 // Export a named `lusitana` font instance used across the UI components.
 export const lusitana = Lusitana({
   subsets: ['latin'],
-  weight: ['400'],
+  weight: ['400', '700'],
   display: 'swap',
 });
+
+export const inter = Inter({ subsets: ['latin'] });
